e0e225f53f29d8e9eb074fbf124aeafed30689c2
Ayman <nassriayman@gmail.com>
2023-09-07 06:33:30 -0700

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * First C Program 
+ * */
+
+int main(void)
+{
+	char *message = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
