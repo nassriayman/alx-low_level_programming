c80587b468cf3199bcd0379d3a15eadb8519f0e1
Ayman <nassriayman@gmail.com>
2023-09-08 04:39:46 -0700

    First var task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+ * main - A program
  *
  * Return: Always 0 (Success)
  */
